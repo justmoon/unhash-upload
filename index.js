647ba70350740522a05dc4b580cc7a83074b2f04
Stefan Thomas <justmoon@members.fsf.org>
2017-09-06 21:26:40 -0700

    feat: pay for upload

--- index.js
+++ index.js
@@ -1,31 +1,50 @@
 'use strict'
 
 const fs = require('mz/fs')
-const fetch = require('node-fetch')
+const request = require('superagent')
+const streamifier = require('streamifier')
 
-const HOSTS = [
-  // 'https://unhash.link',
-  'https://unhash.s3.amazonaws.com'
+const Plugin = require(process.env.UNHASH_ILP_PLUGIN || 'ilp-plugin-xrp-escrow')
+
+// UNHASH_ILP_CREDENTIALS should look like this:
+// {
+//   secret: 'snGu...',
+//   server: 'wss://s.altnet.rippletest.net:51233'
+// }
+const ilpCredentials = JSON.parse(process.env.UNHASH_ILP_CREDENTIALS)
+
+const maxPrice = 100
+
+const plugin = new Plugin(ilpCredentials)
+
+require('superagent-ilp')(request, plugin)
+
+const HOSTS = (process.env.UNHASH_HOSTS && JSON.parse(process.env.UNHASH_HOSTS)) || [
+  'https://unhash.link'
 ]
 
-const uploadFromBuffer = (buffer, opts) => {
+const uploadFromStream = (stream, opts) => {
   // Upload to all of the hosts
   return Promise.all(HOSTS.map(host => {
     // Look up the upload URI from metadata
     return getUploadUri(host)
       .then(uri => {
         // POST the actual file buffer
-        return fetch(uri, {
-          method: 'POST',
-          body: buffer
-        })
+        const req = request
+          .post(uri)
+          .pay(maxPrice)
+          .type('application/octet-stream')
+
+        stream.pipe(req, { end: false })
+
+        return new Promise(resolve => stream.on('end', () => resolve(req)))
       })
-      .then(res => res.json())
-      .then(json => {
+      .then(res => {
         // For each result, let's add the hostname as a property
-        return Object.assign({}, json, {
-          host
-        })
+        return {
+          host,
+          uri: host + '/' + res.body.digest
+        }
       })
   }))
     .then(hosts => {
@@ -35,20 +54,53 @@ const uploadFromBuffer = (buffer, opts) => {
     })
 }
 
+const uploadFromBuffer = (buffer, opts) => {
+  // Upload to all of the hosts
+  return Promise.all(HOSTS.map(host => {
+    // Look up the upload URI from metadata
+    return getUploadUri(host)
+      .then(uri => {
+        // POST the actual file buffer
+        return request
+          .post(uri)
+          .send(buffer)
+          .pay(maxPrice)
+          .type('application/octet-stream')
+      })
+      .then(res => {
+        // For each result, let's add the hostname as a property
+        return {
+          host,
+          uri: host + '/' + res.body.digest
+        }
+      })
+  }))
+    .then(hosts => {
+      return {
+        hosts
+      }
+    })
+
+  return uploadFromStream(streamifier.createReadStream(buffer), opts)
+}
+
 const uploadFromFile = (filename, opts) => {
-  return fs.readFile(filename)
-    .then(contents => uploadFromBuffer(contents, opts))
+  // const stream = fs.createReadStream(filename)
+  // return uploadFromStream(stream, opts)
+  const buffer = fs.readFileSync(filename)
+  return uploadFromBuffer(buffer, opts)
 }
 
 const getUploadUri = host => {
-  return fetch(`${host}/.well-known/unhash.json`)
-    .then(res => res.json())
-    .then(json => {
-      return json.upload
+  return request
+    .get(`${host}/.well-known/unhash.json`)
+    .then(res => {
+      return res.body.upload
     })
 }
 
 Object.assign(exports, {
+  uploadFromStream,
   uploadFromBuffer,
   uploadFromFile,
   getUploadUri
